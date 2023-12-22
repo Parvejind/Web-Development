a1d67fca64e85365a2ce4480d836253d76c163c3
MD PARVEJ <136992647+Parvejind@users.noreply.github.com>
2023-12-22 15:16:03 +0530

    Delete PYTHON directory

--- PYTHON/sequences.py
+++
@@ -1,9 +0,0 @@
-names = ["Harry", "Ron", "Hermione"]
-print(names[2])
-
-name = "Harry"
-print(name[4])
-
-coordinateX = 10.0
-coordinateY = 20.0
-coordinate = (10.0, 20.0)
--- PYTHON/squares.py
+++
@@ -1,7 +0,0 @@
-from functions import square
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")
-
-p = Point(2, 8)
-print(p.x)
-print(p.y)
