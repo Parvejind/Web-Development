ac293ca75e461df31c092813783ff732e04e6940
Parvejind <mparvejind@gmail.com>
2023-12-24 00:28:11 +0530

    Add

---
+++ JavaScript/server.js
@@ -0,0 +1,42 @@
+// Hello World in Nodejs
+console.log(this)
+
+// Values and Datatypes
+
+// - primitive Data Types
+
+// - number: 2, 3, 4, 4.5, 24
+// - string: "PW Skills" or 'PW Skills'
+// - Boolean: true, fals3
+// - null 
+// - undefined
+
+// - Non Primitive Data Types
+// - Array
+
+let name1 = "Raju"
+let name2  = "Parsa"
+let name3 = "Bester"
+let name4 = "Kaif"
+
+let names = ["Raju", "Parsa", "Bester", "Kaif", 2, 3, false, "Raju"]
+
+// - Object: {}
+
+let myName = "Raju"
+
+myName = "Mr. Raju"
+
+// Assignment operator
+let highScore = 300
+console.log(highScrore)
+
+let pwSkillsCoursePrice = 3500
+console.log(pwSkillsCoursePrice);
+
+pwSkillsCoursePrice = 2000
+console.log(pwSkillsCoursePrice);
+
+let userName = "Raju"
+
+let userBankbalance = 5000 // this is a bank balance
